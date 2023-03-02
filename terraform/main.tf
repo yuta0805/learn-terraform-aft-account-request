f81aa32f711ac74ff56eaa60927d5c5a3618eeaf
yuta0805 <yuta-hamada@freee.co.jp>
2023-03-02 19:06:47 +0900

    create account for kakehashi02

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,26 +2,21 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
+    AccountEmail              = "yuta-hamada+kh02@freee.co.jp"
+    AccountName               = "kakehashi01"
+    ManagedOrganizationalUnit = "demo01"
+    SSOUserEmail              = "yuta-hamada+kh02@freee.co.jp"
+    SSOUserFirstName          = "yuta"
+    SSOUserLastName           = "kakehashi01"
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Stage"   = "dev"
+    "project" = "kakehashi02"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
+    change_requested_by = "AFT learn"
     change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
   }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
 }
